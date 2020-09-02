1165110f65c03715fb5efe50381d37a131b20f24
AliabbasVohra <aliabbasvohra@gmail.com>
2020-09-02 15:23:59 +0530

    enabled preloding strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'
+import { Routes, RouterModule, PreloadAllModules, NoPreloading, PreloadingStrategy } from '@angular/router'
+import { CustomPreloadingService } from './custom-preloading.service';
 
 const routes: Routes =
   [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-    { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
-    { path: 'pipes', loadChildren: () => import('./pipes/pipes.module').then(m => m.PipeModule) }
+    {
+      path: 'home',
+      loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+      data: { 'preload': true }
+    },
+    {
+      path: 'about',
+      loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+    },
+    {
+      path: 'pipes',
+      loadChildren: () => import('./pipes/pipes.module').then(m => m.PipeModule)
+    }
   ]
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes,
+      // { preloadingStrategy: NoPreloading }
+      // { preloadingStrategy: PreloadAllModules }
+      { preloadingStrategy: CustomPreloadingService }
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
